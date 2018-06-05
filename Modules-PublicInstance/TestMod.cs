2e178a2f2dc399ed8d0a2030e01b876491582e82
Noikoio <noikoio1@gmail.com>
2018-06-04 19:23:00 -0700

    Reorganized solution projects
    Clear separation between what features will be available in the public
    instance and additional features available for a self-hosted instance.

---
+++ Modules-PublicInstance/TestMod.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Discord.WebSocket;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+
+namespace Kerobot.Modules
+{
+    [KerobotModule]
+    class TestMod : ModuleBase
+    {
+        public TestMod(Kerobot kb) : base(kb)
+        {
+            kb.DiscordClient.MessageReceived += DiscordClient_MessageReceived;
+        }
+
+        private async Task DiscordClient_MessageReceived(SocketMessage arg)
+        {
+            if (arg.Content.ToLower() == ".test")
+            {
+                await arg.Channel.SendMessageAsync("I respond to your test.");
+            }
+        }
+
+        public override Task<object> CreateGuildStateAsync(JToken config)
+            => Task.FromResult<object>(new Dictionary<ulong, string>());
+    }
+}
